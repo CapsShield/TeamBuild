7311c4bf4cbfa85b06b9e4f22fd1266fe8a2f8c9
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-26 17:42:35 -0600

    wrapped whole app in centering flexbo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,13 +5,19 @@ import Sidebar from './Sidebar.jsx';
 
 export default function App() {
   return (
-    <GridContainer>
-      <MoreLike />
-      <Sidebar />
-    </GridContainer>
+    <Center>
+      <GridContainer>
+        <MoreLike />
+        <Sidebar />
+      </GridContainer>
+    </Center>
   );
 }
 
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: 616px 16px 308px;
