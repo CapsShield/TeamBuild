bf3eca80564857f488459805601b683938e837d6
joelsrubin <joelsrubin@gmail.com>
2021-01-28 12:37:35 -0600

    comprssion added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
-
+app.use(compression());
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
