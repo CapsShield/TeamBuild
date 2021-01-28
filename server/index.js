5d288c76c9f5055b446412183fb629344a46cbc5
joelsrubin <joelsrubin@gmail.com>
2021-01-28 13:11:23 -0600

    compression order changed

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
-
-app.use(express.static(path.join(__dirname, '..', 'dist')));
 app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
